5d42862e0ac5cd807c30dfabeca261c285b38474
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-04-20 13:10:24 +1200

    initialize: webpack, babel, typescript

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# crypto-pairs
+# Crypto Pairs 
+
+An application that displays cryptocurrency pair values.
+
+```
+User Story
+
+AS a CC investor / trader
+I WANT to clearly view changes in the market
+SO I can make an investment decision
+```
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+
+module.exports={
+    mode: "development", 
+    entry: "./src/index.tsx", 
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "main.js"
+    },
+    target: "web",
+    devServer: {
+        port: "9500",
+        static: ["./public"],
+        /** "open" 
+         * opens the browser after server is successfully started
+        */
+        open: true,
+        /** "hot"
+         * enabling and disabling HMR. takes "true", "false" and "only". 
+         * "only" is used if enable Hot Module Replacement without page 
+         * refresh as a fallback in case of build failures
+         */
+        hot: true ,
+        /** "liveReload"
+         * disable live reload on the browser. "hot" must be set to false for this to work
+        */
+        liveReload: true
+    },
+    resolve: {
+        /** "extensions" 
+         * If multiple files share the same name but have different extensions, webpack will 
+         * resolve the one with the extension listed first in the array and skip the rest. 
+         * This is what enables users to leave off the extension when importing
+         */
+        extensions: ['.tsx','.ts','.jsx','.js'] 
+    },
+    module:{
+        /** "rules"
+         * This says - "Hey webpack compiler, when you come across a path that resolves to a '.js or .jsx' 
+         * file inside of a require()/import statement, use the babel-loader to transform it before you 
+         * add it to the bundle. And in this process, kindly make sure to exclude node_modules folder from 
+         * being searched"
+         */
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,    //kind of file extension this rule should look for and apply in test
+                exclude: /node_modules/, //folder to be excluded
+                use:  'babel-loader' //loader which we are going to use
+            }
+        ]
+    }
+};
