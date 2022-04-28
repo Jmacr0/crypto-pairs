8d505bc0fd1fc8dab1cd3117c74ed3d168fcd399
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-04-29 02:45:50 +1000

    add mobile nav and router switch components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import "bulma/css/bulma.min.css";
+import MobileNavigation from "./components/MobileNavigation";
+import RouterSwitch from "./RouterSwitch";
 
 const App = () =>{
-    return (
-        <>
-            <h1>
+	return (
+		<>
+			<h1>
                 Welcome to React App thats build using Webpack and Babel separately
-            </h1>
-        </>
-    )
+			</h1>
+			<RouterSwitch />
+			<MobileNavigation />
+		</>
+	);
 };
 
 export default App;
