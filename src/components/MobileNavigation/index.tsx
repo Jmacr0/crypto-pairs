7bf790e5ed141464cd6b6b109ac0e4c6100e8b09
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-04-29 23:07:17 +1000

    add handle navigation selection.

--- src/components/MobileNavigation/index.tsx
+++ src/components/MobileNavigation/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
@@ -9,10 +10,30 @@ import HomeIcon from "@mui/icons-material/Home";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 
 const MobileNavigation = () => {
+	// React-router hook to change URL.
+	const navigate = useNavigate();
+
+	const handleNavigationChange = (_e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+		switch (index) {
+		case 0:
+			navigate("/favourite/");
+			break;
+		case 1:
+			navigate("/");
+			break;
+		case 2:
+			navigate("/dash/");
+			break;
+		default:
+			break;
+		}
+	};
+
 	return(
 		<Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }} elevation={3}>
 			<BottomNavigation 
 				showLabels
+				onChange={handleNavigationChange}
 			>
 				<BottomNavigationAction label="Fave" icon={<FavoriteIcon sx={{ fontSize: 25 }}/>} />
 				<BottomNavigationAction label="Home" icon={<HomeIcon sx={{ fontSize: 25 }}/>} />
