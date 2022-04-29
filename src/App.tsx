344297ba96902259da8118dae069654c0f79c667
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-04-29 23:06:29 +1000

    move mobile navigation to router switch component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import MobileNavigation from "./components/MobileNavigation";
 import RouterSwitch from "./RouterSwitch";
 
 const App = () =>{
 	return (
 		<>
-			<h1>
-                Welcome to React App thats build using Webpack and Babel separately
-			</h1>
 			<RouterSwitch />
-			<MobileNavigation />
 		</>
 	);
 };
