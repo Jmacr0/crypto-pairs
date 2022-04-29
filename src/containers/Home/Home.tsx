b9545eab9b083a5780b8dfde51830905aa736952
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-04-29 23:07:40 +1000

    init home container.

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<h1>
+			Welcome to Crypto Pairs React app thats build using Webpack and Babel separately
+		</h1> 
+	);
+};
+
+export default Home;
