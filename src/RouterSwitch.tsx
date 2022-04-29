daee94ad1ac43fdc0f8e149354f58187b0c72355
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-04-29 23:05:57 +1000

    add main routesl. moved mobile navigation to component.

--- src/RouterSwitch.tsx
+++ src/RouterSwitch.tsx
@@ -3,15 +3,19 @@ import {
 	BrowserRouter as Router,
 	Routes,
 	Route,
-	Link
 } from "react-router-dom";
+import Home from "./containers/Home/Home";
+import MobileNavigation from "./components/MobileNavigation";
 
 const RouterSwitch = () => {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<p>home</p>}/>
+				<Route path="/favourite/" element={<Home />}/>
+				<Route path="/dash/" element={<Home />}/>
+				<Route path="/" element={<Home />}/>
 			</Routes>
+			<MobileNavigation />
 		</Router>
 	);
 };
