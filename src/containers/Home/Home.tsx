154b80412078eda4876ff75d81d170bafe7fda5f
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-05-04 20:19:05 +1000

    add button send req to server for CMC data.

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 const Home = () => {
+	const handleClick = async () => {
+		const { data } = await fetch("/api/cmc")
+			.then(response => {
+				return response.json();
+			});
+		console.log(data);
+	};
 	return (
-		<h1>
-			Welcome to Crypto Pairs React app thats build using Webpack and Babel separately
-		</h1> 
+		<>
+			<h1>
+				Welcome to Crypto Pairs React app thats build using Webpack and Babel separately
+			</h1> 
+			<button onClick={handleClick}>request CMC</button>
+		</>
 	);
 };
 
