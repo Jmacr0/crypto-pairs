3abd80f27fa5e52317080a18f84180c15908d7e4
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-05-12 00:13:47 +1000

    add redux provider / store.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import * as serviceWorker from "./serviceWorkerRegistration";
 
+import "./index.css";
+
+import store from "./store/store";
+import { Provider } from "react-redux";
+
 const container = document.getElementById("root");
 const root = createRoot(container); 
 
-root.render(<App/>);
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
 
 if(process.env.NODE_ENV === "production") serviceWorker.register();
 if(process.env.NODE_ENV === "development") serviceWorker.unregister();
