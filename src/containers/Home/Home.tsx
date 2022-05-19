55e45ba061886d770d19d227aa70e08f7458c208
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-05-19 23:02:23 +1000

    add SearchBar to Home comp.

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,19 +1,10 @@
 import React from "react";
+import SearchBar from "../../components/SearchBar";
 
 const Home = () => {
-	const handleClick = async () => {
-		const { data } = await fetch("/api/cmc")
-			.then(response => {
-				return response.json();
-			});
-		console.log(data);
-	};
 	return (
 		<>
-			<h1>
-				Welcome to Crypto Pairs React app thats build using Webpack and Babel separately
-			</h1> 
-			<button onClick={handleClick}>request CMC</button>
+			<SearchBar/>
 		</>
 	);
 };
