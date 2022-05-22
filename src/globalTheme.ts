765b3ee8bdf77d74bc4c4f093f609bda7cae6deb
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-05-22 21:05:33 +1000

    init globalTheme to modify MUI theme.

---
+++ src/globalTheme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#000",
+		},
+		secondary: {
+			main: "#c7c7c7"
+		},
+	},
+});
+
+export default theme;
