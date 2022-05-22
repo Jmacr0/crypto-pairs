654771bdf9ea2ed9f1d5b128638058d0b17325fc
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-05-22 21:06:11 +1000

    change rendered component.

--- src/RouterSwitch.tsx
+++ src/RouterSwitch.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 import {
 	BrowserRouter as Router,
 	Routes,
 	Route,
 } from "react-router-dom";
+import Fave from "./containers/Fave/Fave";
+import Dash from "./containers/Dash/Dash";
 import Home from "./containers/Home/Home";
 import MobileNavigation from "./components/MobileNavigation";
 
@@ -11,8 +12,8 @@ const RouterSwitch = () => {
 	return (
 		<Router basename={process.env.BASE_URL}>
 			<Routes>
-				<Route path="/favourite" element={<Home />}/>
-				<Route path="/dash" element={<Home />}/>
+				<Route path="/favourite" element={<Fave />}/>
+				<Route path="/dash" element={<Dash />}/>
 				<Route path="/" element={<Home />}/>
 			</Routes>
 			<MobileNavigation />
