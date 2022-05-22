f99df9b84bfbf52c73a99337c091b0dbe5aebedf
Jon Anthony Macrohon <jonmacrohon@gmail.com>
2022-05-22 21:19:56 +1000

    use Typography and add loading gif.

---
+++ src/containers/Dash/Dash.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+
+const Dash = () =>{
+	return (
+		<>
+			<Typography variant="h4">
+            Dash in progress.
+			</Typography>
+			<img 
+				width="100%"
+				src="https://64.media.tumblr.com/bcd0ab382d4db957589462a2876ba3dd/1ea0df0b0150f330-75/s540x810/2b86c16f8006cd9117e2646031b5ffd1c64f7f74.gifv" 
+				alt="going-merry" 
+			/>
+		</>
+	);
+};
+
+export default Dash;
